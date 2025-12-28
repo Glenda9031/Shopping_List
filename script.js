d1da83425db55ab21ae2d28ee6d299d4dea62cc4
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-12-27 18:32:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,93 @@ function displayItems() {
     checkUI();
 }
 
+function onAddItemSubmit(e) {
+    e.preventDefault();
+
+    // Trim the input value to to remove whitespace - disallowing duplicate items due to whitespace in process
+    const newItem = itemInput.value.trim();
+    
+    // Validate input
+    if (newItem === '') {
+        alert('Please enter an item');
+        return;
+    }
+
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    } else {
+        if (checkIfItemExists(newItem)) {
+            alert(`The item "${newItem}" already exists!`);
+            return;
+        }
+    }
+
+    // Create item DOM element
+    addItemToStorage(newItem);
+
+    checkUI();
+
+    itemInput.value = '';
+}
+
+function addItemToDOM(item) {
+    // Create list item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    // Add li to the DOM
+    itemList.appendChild(li);
+}
+
+function createButton(classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+function addItemToStorage(item) {
+    const itemsFromStorage = getItemsFromStorage();
+
+    // Add new item to array
+    itemsFromStorage.push(item);
+
+    // Convert to JSON string and set to local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage() {
+    let itemsFromStorage;
+
+    if (localStorage.getItem('items') === null) {
+        itemsFromStorage = [];
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+    }
+
+    return itemsFromStorage;
+}
+
+function onClickItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        removeItem(e.target.parentElement.parentElement);
+    } else if (e.target.closest('li')) {
+        
+    }
+}
